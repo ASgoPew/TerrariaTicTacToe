ad4065b0677334018be81ad0b1368f0ae2b91537
ASgoPew <35419712+ASgoPew@users.noreply.github.com>
2020-02-04 01:31:43 +0300

    The game plugin itself

---
+++ TUITicTacToe/TicTacToe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using TerrariaUI.Base;
+using TerrariaUI.Base.Style;
+using TerrariaUI.Widgets;
+
+namespace TUITicTacToe
+{
+    public class TicTacToe : VisualObject
+    {
+        public TicTacToe()
+            : base(0, 0, 0, 0, null, null, null)
+        {
+            SetupGrid(new ISize[] { new Relative(33), new Relative(33), new Relative(34) },
+                new ISize[] { new Relative(33), new Relative(33), new Relative(34) });
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                {
+                    int cellType = (i + j) % 2;
+                    this[i, j] = new VisualContainer(0, 0, 0, 0, null, new ContainerStyle()
+                    {
+                        Wall = cellType == 0 ? WallID.AmberGemspark : WallID.AmethystGemspark,
+                        WallColor = PaintID.White
+                    }, callback: (self, touch) =>
+                    {
+                        int turn = (int)self.Parent["turn"];
+                        self["value"] = turn;
+                        Image image = self.Add(new Image(0, 0, $"worldedit\\schematic-{(turn == 0 ? "X" : "O")}.dat")) as Image;
+                        image.Style.TileColor = turn == 0 ? PaintID.DeepBlue : PaintID.DeepRed;
+                        self.Update().Apply().Draw();
+                        self.Parent["turn"] = 1 - turn;
+
+                        // Check if someone has already won the game
+                    });
+                }
+        }
+    }
+}
---
+++ TUITicTacToe/TicTacToePlugin.cs
@@ -0,0 +1,76 @@
+﻿using FakeProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using TerrariaApi.Server;
+using TerrariaUI;
+using TerrariaUI.Base;
+using TerrariaUI.Base.Style;
+using TerrariaUI.Widgets;
+using TShockAPI;
+using TUIPlugin;
+
+namespace TUITicTacToe
+{
+    [ApiVersion(2, 1)]
+    public class TicTacToePlugin : TerrariaPlugin
+    {
+        public TicTacToePlugin(Main game) : base(game)
+        {
+        }
+
+        public override string Name => base.Name;
+
+        public override Version Version => base.Version;
+
+        public override string Author => base.Author;
+
+        public override string Description => base.Description;
+
+        public override void Initialize()
+        {
+            // Determine the position and size of the interface.
+            int x = 100, y = 100, w = 50, h = 30;
+            // Pass an empty provider to the panel (the interface will be drawn on Main.tile).
+            object provider = FakeProviderAPI.CreateReadonlyTileProvider("TicTacToe", x, y, w, h);
+            // Although we can use as a provider, for example, FakeTileRectangle from FakeManager:
+            //object provider = FakeManager.FakeManager.Common.Add("TestPanelProvider", x, y, w, h);
+
+            // Create a panel with a wall of diamond gemspark wall with black paint.
+            Panel root = TUI.Create(new Panel("TicTacToe", x, y, w, h, null,
+                new ContainerStyle() { Wall = WallID.DiamondGemspark, WallColor = PaintID.White }, provider)) as Panel;
+
+            root.SetupGrid(new ISize[] { new Absolute(20), new Relative(100) });
+
+            var menu = root[0, 0] = new VisualContainer(new ContainerStyle() { WallColor = PaintID.Black });
+            menu.SetupLayout(Alignment.Up, Direction.Down, Side.Center, null, 0);
+            menu.AddToLayout(new Button(0, 0, 20, 4, "Start", null, new ButtonStyle()
+            {
+                Wall = WallID.AmberGemspark,
+                WallColor = PaintID.DeepGreen
+            }, (self, touch) =>
+            {
+                for (int i = 0; i < root[1, 0].ChildCount; i++)
+                    root[1, 0].GetChild(i).RemoveAll();
+                root[1, 0].Update().Apply().Draw();
+            }));
+            menu.AddToLayout(new Button(0, 0, 20, 4, "help", null, new ButtonStyle()
+            {
+                Wall = WallID.AmberGemspark,
+                WallColor = PaintID.Gray
+            }, (self, touch) => TSPlayer.All.SendInfoMessage("HELP")));
+
+            var game = root[1, 0] = new TicTacToe();
+            game["turn"] = 0;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
